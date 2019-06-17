dc9eb4415af6a4776217bb63a27e2a53d2a8f480
Hans Krohn <hanskrohn70@gmail.com>
2019-06-17 10:47:37 -0500

    Finished setting up routes and corrected username, password validation for admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,5 @@ class Admin < ApplicationRecord
     has_many :teachers, through: :locations
     has_many :enrolls, dependent: :destroy
     has_many :students, through: :enrolls
+    has_secure_password
 end
